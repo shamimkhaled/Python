cd2c32ea10b5d900dc670db208325d968297db96
Shamim Khaled <71558919+shamimkhaled@users.noreply.github.com>
2022-12-30 19:29:23 +0600

    Delete mysite directory

--- mysite/food/forms.py
+++
@@ -1,7 +0,0 @@
-from .models import Item
-from django import forms
-
-class ItemForm(forms.ModelForm):
-    class Meta:
-        model = Item
-        fields = ['item_name', 'item_desc', 'item_price', 'item_image']
